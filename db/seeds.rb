b6274df0b576ef1ce58ff1eb845489646e7656ec
CORIVER <152018733+CORICORICORIVER@users.noreply.github.com>
2024-09-02 21:10:21 +0900

    20240902ver8
    ログインしたらユーザーページに飛べるよう設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
+
+CSV.foreach('db/seeds/csv/content.csv', headers: true) do |row|
+  Content.create(
+    title: row['title'],
+    image_url :row['image_url'],
+    director: row['director'],
+    description: row['description'],
+    open_date: row['open_date']
+  )
+end
